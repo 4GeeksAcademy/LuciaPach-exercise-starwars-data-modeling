e3b152a6aaf19fb09da18c162fe2f4ee66e466f9
Lucía Pacheco Daldés <150815101+Emerydith@users.noreply.github.com>
2024-03-16 10:51:53 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -13,13 +13,12 @@ class Usuarios(Base):
     name = Column(String(250))
     model = Column(String(250))
     pasajeros = Column(Integer, nullable=False)
-    favoritos_id = Column(Integer, ForeignKey('favoritos.id'))
 class Favoritos(Base):
     __tablename__ = 'favoritos'
     id = Column(Integer, primary_key=True)
     person_id = Column(Integer, ForeignKey('people.id'))
     planets_id = Column(Integer, ForeignKey('planets.id'))
-    vehiculos_id = Column(Integer, ForeignKey('veiculos.id'))
+    vehiculos_id = Column(Integer, ForeignKey('vehiculos.id'))
     usuarios_id = Column(Integer, ForeignKey('usuarios.id'))
 class People(Base):
     __tablename__ = 'people'
@@ -27,21 +26,18 @@ class People(Base):
     name = Column(String(250), nullable=False)
     eye_color = Column(String(250), nullable=False)
     height = Column(Integer, nullable=False)
-    favoritos_id = Column(Integer, ForeignKey('favoritos.id'))
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
     climate = Column(String(250))
     population = Column(Integer, nullable=False)
-    favoritos_id = Column(Integer, ForeignKey('favoritos.id'))
 class Vehiculos(Base):
     __tablename__ = 'vehiculos'
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
     model = Column(String(250))
     pasajeros = Column(Integer, nullable=False)
-    favoritos_id = Column(Integer, ForeignKey('favoritos.id'))
 
     def to_dict(self):
         return {}
